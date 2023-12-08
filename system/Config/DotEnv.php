3514dbabb1211c0113e7485f954a6e6e29f0897c
Juan Manuel <xomie_01@hotmail.com>
2023-12-08 20:48:40 -0300

    Update DotEnv

--- system/Config/DotEnv.php
+++ system/Config/DotEnv.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * This file is part of CodeIgniter 4 framework.
+ * This file is part of AssisterMicro 1 framework.
  *
- * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ * (c) CodeIgniter Foundation <support@hexome.cloud>
  *
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
@@ -30,7 +30,32 @@ class DotEnv
      */
     public function __construct(string $path, string $file = '.env')
     {
-        $this->path = rtrim($path, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . $file;
+        $this->setPath($path,$file);
+    }
+
+    private function getDirectory(): ?string {
+        return $this->directory;
+    }
+    private function getFile() : ?string {
+        return $this->file;
+    }
+
+    public function setPath(?string $path,?string $file ) : void {
+        $this->setFile($file);
+        $this->setDirectory($path);
+        $this->inicializatePath();
+    }
+
+    private function inicializatePath() : void {
+        $this->path = $this->getDirectory() . DIRECTORY_SEPARATOR . $this->getFile();
+    }
+
+    private function setDirectory(?string $directory ) : void {
+        $this->directory = rtrim($directory, DIRECTORY_SEPARATOR);
+    }
+
+    private function setFile( ?string $file ) : void {
+        $this->file = $file;
     }
 
     /**
@@ -50,34 +75,41 @@ class DotEnv
      */
     public function parse(): ?array
     {
-        // We don't want to enforce the presence of a .env file, they should be optional.
-        if (! is_file($this->path)) {
+        // Check if the .env file exists, return null if not
+        if (!is_file($this->path)) {
             return null;
         }
 
-        // Ensure the file is readable
-        if (! is_readable($this->path)) {
+        // Ensure the file is readable, throw an exception if it's not
+        if (!is_readable($this->path)) {
             throw new InvalidArgumentException("The .env file is not readable: {$this->path}");
         }
 
-        $vars = [];
+        $vars = []; // Initialize an empty array to store variables
 
+        // Read the lines from the .env file, ignoring empty lines
         $lines = file($this->path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
         foreach ($lines as $line) {
-            // Is it a comment?
+            // Skip comments (lines starting with '#')
             if (strpos(trim($line), '#') === 0) {
                 continue;
             }
 
-            // If there is an equal sign, then we know we are assigning a variable.
+            // If the line contains an equal sign, it's a variable assignment
             if (strpos($line, '=') !== false) {
+                // Normalize the variable and extract its name and value
                 [$name, $value] = $this->normaliseVariable($line);
-                $vars[$name]    = $value;
+
+                // Store the variable in the $vars array
+                $vars[$name] = $value;
+
+                // Set the variable in the environment
                 $this->setVariable($name, $value);
             }
         }
 
+        // Return the array of parsed variables
         return $vars;
     }
 
@@ -107,22 +139,25 @@ class DotEnv
      */
     public function normaliseVariable(string $name, string $value = ''): array
     {
-        // Split our compound string into its parts.
+        // Split our compound string into its parts if there's an equal sign
         if (strpos($name, '=') !== false) {
+            // Explode the string into $name and $value
             [$name, $value] = explode('=', $name, 2);
         }
 
+        // Trim leading and trailing whitespace from $name and $value
         $name  = trim($name);
         $value = trim($value);
 
-        // Sanitize the name
+        // Sanitize the name by removing 'export' keyword and quotes
         $name = preg_replace('/^export[ \t]++(\S+)/', '$1', $name);
         $name = str_replace(['\'', '"'], '', $name);
 
-        // Sanitize the value
+        // Sanitize the value by handling quotes and nested variables
         $value = $this->sanitizeValue($value);
         $value = $this->resolveNestedVariables($value);
 
+        // Return the sanitized $name and $value as an array
         return [$name, $value];
     }
 
@@ -136,45 +171,53 @@ class DotEnv
      */
     protected function sanitizeValue(string $value): string
     {
-        if (! $value) {
-            return $value;
-        }
-
-        // Does it begin with a quote?
-        if (strpbrk($value[0], '"\'') !== false) {
-            // value starts with a quote
-            $quote = $value[0];
-
-            $regexPattern = sprintf(
-                '/^
-                %1$s          # match a quote at the start of the value
-                (             # capturing sub-pattern used
-                 (?:          # we do not need to capture this
-                 [^%1$s\\\\] # any character other than a quote or backslash
-                 |\\\\\\\\   # or two backslashes together
-                 |\\\\%1$s   # or an escaped quote e.g \"
-                 )*           # as many characters that match the previous rules
-                )             # end of the capturing sub-pattern
-                %1$s          # and the closing quote
-                .*$           # and discard any string after the closing quote
-                /mx',
-                $quote
-            );
-
-            $value = preg_replace($regexPattern, '$1', $value);
-            $value = str_replace("\\{$quote}", $quote, $value);
-            $value = str_replace('\\\\', '\\', $value);
-        } else {
-            $parts = explode(' #', $value, 2);
-            $value = trim($parts[0]);
-
-            // Unquoted values cannot contain whitespace
-            if (preg_match('/\s+/', $value) > 0) {
-                throw new InvalidArgumentException('.env values containing spaces must be surrounded by quotes.');
+            // If the value is empty, return it as is
+            if (! $value) {
+                return $value;
             }
-        }
 
-        return $value;
+            // Check if the value starts with a quote
+            if (strpbrk($value[0], '"\'') !== false) {
+                // Get the starting quote character
+                $quote = $value[0];
+
+                // Define the regular expression pattern to match quoted strings
+                $regexPattern = sprintf(
+                    '/^
+                    %1$s          # Match a quote at the start of the value
+                    (             # Capturing sub-pattern used
+                    (?:          # We do not need to capture this
+                    [^%1$s\\\\] # Any character other than a quote or backslash
+                    |\\\\\\\\   # Or two backslashes together
+                    |\\\\%1$s   # Or an escaped quote e.g \"
+                    )*           # As many characters that match the previous rules
+                    )             # End of the capturing sub-pattern
+                    %1$s          # And the closing quote
+                    .*$           # And discard any string after the closing quote
+                    /mx',
+                    $quote
+                );
+
+                // Extract the quoted value and handle escape sequences
+                $value = preg_replace($regexPattern, '$1', $value);
+                $value = str_replace("\\{$quote}", $quote, $value);
+                $value = str_replace('\\\\', '\\', $value);
+            } else {
+                // Split the value at the first '#'
+                $parts = explode(' #', $value, 2);
+                // Take the trimmed value before '#'
+                $value = trim($parts[0]);
+
+                // Unquoted values cannot contain whitespace
+                if (preg_match('/\s+/', $value) > 0) {
+                    // Throw an exception for unquoted values with spaces
+                    throw new InvalidArgumentException('.env values containing spaces must be surrounded by quotes.');
+                }
+            }
+
+            // Return the processed value
+            return $value;
+
     }
 
     /**
@@ -188,23 +231,28 @@ class DotEnv
      */
     protected function resolveNestedVariables(string $value): string
     {
-        if (strpos($value, '$') !== false) {
-            $value = preg_replace_callback(
-                '/\${([a-zA-Z0-9_\.]+)}/',
-                function ($matchedPatterns) {
-                    $nestedVariable = $this->getVariable($matchedPatterns[1]);
+            // Check if the value contains '${}' patterns
+            if (strpos($value, '$') !== false) {
+                // Replace ${} patterns with corresponding values
+                $value = preg_replace_callback(
+                    '/\${([a-zA-Z0-9_\.]+)}/',
+                    function ($matchedPatterns) {
+                        $nestedVariable = $this->getVariable($matchedPatterns[1]);
 
-                    if ($nestedVariable === null) {
-                        return $matchedPatterns[0];
-                    }
+                        // If the nested variable doesn't exist, return the original pattern
+                        if ($nestedVariable === null) {
+                            return $matchedPatterns[0];
+                        }
 
-                    return $nestedVariable;
-                },
-                $value
-            );
-        }
+                        // Return the value of the nested variable
+                        return $nestedVariable;
+                    },
+                    $value
+                );
+            }
 
-        return $value;
+            // Return the processed value
+            return $value;
     }
 
     /**
@@ -215,20 +263,28 @@ class DotEnv
      *
      * @return string|null
      */
-    protected function getVariable(string $name) : ?string
+    protected function getVariable(?string $name) : ?string
     {
-        switch (true) {
-            case array_key_exists($name, $_ENV):
-                return $_ENV[$name];
+        $value = false;
 
-            case array_key_exists($name, $_SERVER):
-                return $_SERVER[$name];
+        $getenv = getenv($name);
 
-            default:
-                $value = getenv($name);
-
-                // switch getenv default to null
-                return $value === false ? null : $value;
+        // Check if the variable exists in $_ENV and set $value accordingly
+        if (array_key_exists($name, $_ENV)) {
+            $value = $_ENV[$name];
         }
+
+        // Check if the variable exists in $_SERVER and update $value if necessary
+        if (!$value && array_key_exists($name, $_SERVER)) {
+            $value = $_SERVER[$name];
+        }
+
+        // If $value is still false, try retrieving it from getenv
+        if (!$value) {
+            $value = $getenv;
+        }
+
+        // If $value is still false, return null; otherwise, return the value
+        return $value !== false ? $value : null;
     }
 }
